659f13f3c0f99592ffd45c7d6ea40f483df15e90
Meenakshiaravind <meenakshiaravind0719@gmail.com>
2025-03-08 18:52:52 -0500

    initial commit

---
+++ automation/src/main/java/com/wisdocity/automation/App.java
@@ -0,0 +1,13 @@
+package com.wisdocity.automation;
+
+/**
+ * Hello world!
+ *
+ */
+public class App
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ automation/src/test/java/com/wisdocity/automation/StandAlone.java
@@ -0,0 +1,28 @@
+package com.wisdocity.automation;
+
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+
+
+
+public class StandAlone {
+	
+	//static WebDriver driver = new ChromeDriver();
+	
+	
+	
+	public static void main(String[] args) {
+			
+		LoginTest Login =new LoginTest();
+	
+		
+		//Homepage Home =new Homepage();
+		//Home.HomePage();
+				//WebDriver driver = new ChromeDriver();
+		//driver.get("https://test.wisdocity.ai/");
+		
+	 
+	}
+}
